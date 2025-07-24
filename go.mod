a149f970d9817e3fb4e1db04726aef9e37334772
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-07-24 13:34:55 +0200

    Ajout de .gitignore pour exclure les fichiers inutiles

--- go.mod
+++ go.mod
@@ -5,4 +5,4 @@ go 1.23.0
 // installer dans la version git precedente
 require github.com/go-chi/chi/v5 v5.2.2 // indirect
 
-// require github.com/go-chi/chi v1.5.5 // indirect
+
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +17,9 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhun.io</a>.")
 }
 
-func faqHandler(w http.ResponseWriter, r *http.Request){
+func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Contente-type", "text/html;charset=utf-8")
-	fmt.Fprint(w,`<H1> FAQ PAGE  </H1> 
+	fmt.Fprint(w, `<H1> FAQ PAGE  </H1> 
 		<ul>
 			<li>
 				<b>IS there a free version</b>
@@ -42,8 +42,6 @@ func faqHandler(w http.ResponseWriter, r *http.Request){
 		`)
 }
 
-
-
 type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -63,7 +61,7 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
-	
+
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
