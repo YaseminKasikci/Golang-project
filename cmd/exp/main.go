a5f5b22a4c735eb0cef6d968b66aaf001baf251f
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-07-25 18:04:41 +0200

    exercice

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 )
 
-type User struct {
-	Name string
-	Bio  string
-	Age  int
+type UserMeta struct {
+	Visit string
 }
 
-type UserMeta struct {
-	Visit int
+type User struct {
+	Name    string
+	Bio     string
+	Age     int
+	Higth   float32
+	Country []UserMeta //slices, arr
+	Like    []int
+	Notes   map[string]int //maps
 }
 
 func main() {
@@ -23,14 +28,32 @@ func main() {
 	}
 
 	user := User{
-		Name: "Suzane Smith",
-		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
-		Age:  123,
+		Name:  "Suzane Smith",
+		Bio:   "<script>alert(\"Haha, you have been h4x0r3d!\");</script>",
+		Age:   123,
+		Higth: 1.89,
+		Country: []UserMeta{
+			{Visit: "France"},
+			{Visit: "Turkey"},
+			{Visit: "Belgium"},
+		},
+		Like: []int{1, 2, 4},
+		Notes: map[string]int{
+			"Math":     13,
+			"Francais": 12,
+			"Art":      15,
+		},
+	}
+
+	if user.Name == "Suzane Smith" {
+		fmt.Printf("---------- OUI---------------")
+	} else {
+		fmt.Printf("----------- YOU NEED TO BE MEMBERS----------------")
 	}
 
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
 		panic(err)
+
 	}
 }
-
