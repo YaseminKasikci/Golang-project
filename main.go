76b2f0c99b008967dedb0857bbe6cb159f3170ff
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-07-29 14:39:48 +0200

    added form for signup

--- main.go
+++ main.go
@@ -29,6 +29,11 @@ func main() {
 		"faq.gohtml", "tailwind.gohtml",
 	))))
 
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"signup.gohtml", "tailwind.gohtml",
+	))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
