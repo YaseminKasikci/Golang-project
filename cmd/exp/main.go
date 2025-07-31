f69f749edaf2c3f6a459190e5f363bedaf2a5fc1
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-07-31 15:34:13 +0200

    connect pgx/sql with go

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
 
 func main() {
-	numbers := []int{1, 2, 3}
-	fmt.Println(numbers[4])
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected!")
 }
