d8271eb0a358bb21ffe05a340ef4c6ac6dd0e672
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-08-07 15:02:07 +0200

    helper function for cookies

--- controllers/users.go
+++ controllers/users.go
@@ -41,16 +41,9 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
-
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/",
-		HttpOnly: true,
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
 	http.Redirect(w, r, "/users/me", http.StatusFound)
-	fmt.Fprintf(w, "User created: %+v", user)
+
 }
 
 func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
@@ -80,23 +73,17 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		http.Error(w, "something went wrong", http.StatusInternalServerError)
 	}
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/",
-		HttpOnly: true,
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
 	http.Redirect(w, r, "/users/me", http.StatusAccepted)
 }
 
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
-	tokenCookie, err := r.Cookie("session")
+	token, err := readCookie(r, CookieSession)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
 	}
-	user, err := u.SessionService.User(tokenCookie.Value)
+	user, err := u.SessionService.User(token)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
