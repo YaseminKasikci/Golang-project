0b064f46d9969d3b5ad5eef25fa777ca9e38d11a
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-06 17:20:27 +0200

    showned err in tpls

--- views/template.go
+++ views/template.go
@@ -34,11 +34,7 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 
 			},
 			"errors": func() []string {
-				return []string{
-					"Don't do that!",
-					"The email address you provided is already associated with an account",
-					"Something went wrong",
-				}
+				return nil
 			},
 		},
 	)
@@ -52,21 +48,12 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-}
-
 type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
+
+func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}, errs ...error) {
 	tpl, err := t.htmlTpl.Clone()
 	if err != nil {
 		log.Printf("cloning template %v", err)
@@ -81,6 +68,13 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 			},
 			"currentUser": func() *models.User {
 				return context.User(r.Context())
+			},
+			"errors": func() []string {
+				var errMessages []string
+				for _, err := range errs {
+					errMessages = append(errMessages, err.Error())
+				}
+				return errMessages
 			},
 		},
 	)
