47d9d61e8771095b9106f509b5fe9fc252325041
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-06 16:56:25 +0200

    download pgconn pgerror

--- models/user.go
+++ models/user.go
@@ -2,10 +2,17 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrEmailTaken = errors.New("models: email address is already in use")
 )
 
 type User struct {
@@ -39,6 +46,12 @@ func (us *UserService) Create(email, password string) (*User, error) {
 		VALUES ($1, $2) RETURNING id;`, email, passwordHash)
 	err = row.Scan(&user.ID)
 	if err != nil {
+		var pgError *pgconn.PgError
+		if errors.As(err, &pgError) {
+			if pgError.Code == pgerrcode.UniqueViolation {
+				return nil, ErrEmailTaken
+			}
+		}
 		return nil, fmt.Errorf("create user: %w", err)
 	}
 	return &user, nil
@@ -79,9 +92,9 @@ func (us *UserService) UpdatePassword(userID int, password string) error {
 	_, err = us.DB.Exec(`
 	UPDATE users 
 	SET password_hash = $2
-	WHERE id = $1;`,  userID, passwordHash)
+	WHERE id = $1;`, userID, passwordHash)
 	if err != nil {
-		 return fmt.Errorf("update password: %w", err)
+		return fmt.Errorf("update password: %w", err)
 	}
 	return nil
 }
