30e51f63c5be4069eeccfa877ba7c73023cac4ae
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-08 17:07:09 +0200

    add filename in Image

--- models/gallery.go
+++ models/gallery.go
@@ -9,7 +9,9 @@ import (
 )
 
 type Image struct {
-	Path string
+	GalleryID int
+	Path      string
+	Filename  string
 }
 
 type Gallery struct {
@@ -121,7 +123,9 @@ func (service *GalleryService) Images(galleryID int) ([]Image, error) {
 	for _, file := range allFiles {
 		if hasExtension(file, service.extensions()) {
 			images = append(images, Image{
+				GalleryID: galleryID,
 				Path: file,
+				Filename: filepath.Base(file),
 			})
 		}
 	}
