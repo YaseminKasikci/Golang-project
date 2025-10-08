cb012154bdf4dc438b3637621bdab252b8805937
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-08 18:08:03 +0200

    fixed escaping image w/ filenameEscaped

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -6,6 +6,7 @@ import (
 	"github/yaseminkasikci/lenslocked/errors"
 	"github/yaseminkasikci/lenslocked/models"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
@@ -58,6 +59,7 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 	type Image struct {
 		GalleryID int
 		Filename  string
+		FilenameEscaped string
 	}
 	var data struct {
 		ID     int
@@ -77,6 +79,7 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		data.Images = append(data.Images, Image{
 			GalleryID: image.GalleryID,
 			Filename:  image.Filename,
+			FilenameEscaped: url.PathEscape(image.Filename),
 		})
 	}
 	g.Templates.Show.Execute(w, r, data)
