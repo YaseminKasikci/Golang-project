5c180a7cc956f16548732f162b04048943d6369f
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-08 16:45:59 +0200

    func image, extension, hasExtension

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,42 +3,42 @@ package main
 import (
 	"fmt"
 	"github/yaseminkasikci/lenslocked/models"
-	"log"
-	"os"
-	"strconv"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	host := os.Getenv("SMTP_HOST")
-	portStr := os.Getenv("SMTP_PORT")
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		panic(err)
-	}
-	username := os.Getenv("SMTP_USERNAME")
-	password := os.Getenv("SMTP_PASSWORD")
-
-	configSMTP := models.SMTPConfig{
-		Host:     host,
-		Port:     port,
-		Username: username,
-		Password: password,
-	}
-	// spew.Dump(configSMTP)
-
-	es := models.NewEmailService(configSMTP)
-	err = es.ForgotPassword(
-		"yasemin.kasikci@ringover.com",
-		"https://lenslocked.com/reset-pw?token=abc123")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Email sent")
+	gs := models.GalleryService{}
+	fmt.Println(gs.Images(2))
 }
+
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	host := os.Getenv("SMTP_HOST")
+// 	portStr := os.Getenv("SMTP_PORT")
+// 	port, err := strconv.Atoi(portStr)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	username := os.Getenv("SMTP_USERNAME")
+// 	password := os.Getenv("SMTP_PASSWORD")
+
+// 	configSMTP := models.SMTPConfig{
+// 		Host:     host,
+// 		Port:     port,
+// 		Username: username,
+// 		Password: password,
+// 	}
+// 	// spew.Dump(configSMTP)
+
+// 	es := models.NewEmailService(configSMTP)
+// 	err = es.ForgotPassword(
+// 		"yasemin.kasikci@ringover.com",
+// 		"https://lenslocked.com/reset-pw?token=abc123")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Email sent")
+// }
