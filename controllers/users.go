270086ef2795dd1f946cc58d8430459ef4d3b947
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-09 17:44:05 +0200

    redirection on galleries instead of user

--- controllers/users.go
+++ controllers/users.go
@@ -58,8 +58,7 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	setCookie(w, CookieSession, session.Token)
-	http.Redirect(w, r, "/users/me", http.StatusFound)
-	// http.Redirect(w, r, "/users/me", http.StatusFound)
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 
 }
 
@@ -96,7 +95,7 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 
 	setCookie(w, CookieSession, session.Token)
 
-	http.Redirect(w, r, "/users/me", http.StatusFound)
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 
 }
 
