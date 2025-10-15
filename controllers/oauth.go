ce064f46cda1cf5aecc12c3b446496520771ef0f
Yasemin Kasikci <yasemin.kasikci@ringover.com>
2025-10-15 16:05:32 +0200

    API calls testing

--- controllers/oauth.go
+++ controllers/oauth.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+
 	"net/http"
 	"strings"
 
@@ -34,6 +38,66 @@ func (oa OAuth) Connect(w http.ResponseWriter, r *http.Request) {
 		// oauth2.S256ChallengeOption(verifier),
 	)
 	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func (oa OAuth) Callback(w http.ResponseWriter, r *http.Request) {
+	provider := chi.URLParam(r, "provider")
+	provider = strings.ToLower(provider)
+	config, ok := oa.ProviderConfigs[provider]
+	if !ok {
+		http.Error(w, "Invalid OAuth2 service", http.StatusBadRequest)
+		return
+	}
+
+	state := r.FormValue("state")
+	cookieState, err := readCookie(r, "oauth_state")
+	if err != nil || cookieState != state {
+		if err != nil {
+			fmt.Println(err)
+		}
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	deleteCookie(w, "oaut_state")
+	code := r.FormValue("code")
+	token, err := config.Exchange(
+		r.Context(),
+		code,
+		oauth2.SetAuthURLParam("redirect_uri", redirectURI(r, provider)))
+	if err != nil {
+		http.Error(w, "Something went wronf un OAUTH C", http.StatusBadRequest)
+	}
+
+	// w.Header().Set("Content-type", "application/json")
+	// enc := json.NewEncoder(w)
+	// enc.SetIndent("", " ")
+	// enc.Encode(token)*
+
+	client := config.Client(r.Context(), token)
+	resp, err := client.Post(
+		"https://api.dropboxapi.com/2/files/list_folder",
+		"application/json",
+		strings.NewReader(`{"path": ""}`),
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	unpretty, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var pretty bytes.Buffer
+	err = json.Indent(&pretty, unpretty, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	pretty.WriteTo(w)
 }
 
 func redirectURI(r *http.Request, provider string) string {
